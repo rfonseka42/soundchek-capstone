c6428433d035e36922bce52f283eb5a8e86a8c4f
rowena fonseka <rafonseka@gmail.com>
2022-09-20 16:10:33 -0400

    fixed issue of api key not working with dotenv file. pushing to develop so api key is not visible

--- src/firebase.js
+++ src/firebase.js
@@ -1,22 +1,18 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
+import { getFirestore } from "@firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCjFwfSsg3d_dN6YTNf7efX34xV339905I",
-  authDomain: "soundchek-capstone.firebaseapp.com",
-  projectId: "soundchek-capstone",
-  storageBucket: "soundchek-capstone.appspot.com",
-  messagingSenderId: "290274258675",
-  appId: "1:290274258675:web:af0da0d022d74d4e555186",
-
-  // apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-  // authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-  // projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-  // storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-  // messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-  // appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
 };
 
 const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
 export const auth = getAuth(app);
-export default app;
+
+// export default app;
