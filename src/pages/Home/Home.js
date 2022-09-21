8647a09466a54aa8ff3ae16f0b67771e1ebd7d26
rowena fonseka <rafonseka@gmail.com>
2022-09-20 20:41:01 -0400

    create get request to firestore db but created infinite loop. Will have to fix when firestore quota is reset

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,12 +1,55 @@
-import React from "react";
+import { useState, useEffect } from "react";
 import Header from "../../components/Header/Header";
 import Hero from "../../components/Hero/Hero";
+import { db } from "../../firebase";
+import { collection, getDocs } from "firebase/firestore";
 
 function Home() {
+  const [rehearsalRooms, setRehearsalRooms] = useState([]);
+  const rehearsalRoomsCollectionRef = collection(db, "rehearsalRooms");
+
+  useEffect(() => {
+    const getRehearsalRooms = async () => {
+      const data = await getDocs(rehearsalRoomsCollectionRef);
+      setRehearsalRooms(
+        data.docs.map((doc) => ({ ...doc.data(), id: doc.id }))
+      );
+    };
+
+    getRehearsalRooms();
+  }, []);
+
   return (
     <>
       <Header />
       <Hero />
+      <h2>Room Details Page</h2>
+      <div className="card-wrap">
+        {rehearsalRooms.map((rehearsalRoom) => {
+          return (
+            <div className="card" key={rehearsalRoom.id}>
+              <div className="card__top">
+                <img
+                  className="card__img"
+                  src={rehearsalRoom.imgUrl}
+                  alt="room"
+                />
+              </div>
+              <div className="card__bottom">
+                <div className="card__bottom--left">
+                  <p>{rehearsalRoom.name}</p>
+                  <p>{rehearsalRoom.address}</p>
+                  <p>{rehearsalRoom.company}</p>
+                  <p>${rehearsalRoom.price}/Hr</p>
+                </div>
+                <div className="card__bottom--right">
+                  <p>{rehearsalRoom.people}</p>
+                </div>
+              </div>
+            </div>
+          );
+        })}
+      </div>
     </>
   );
 }
