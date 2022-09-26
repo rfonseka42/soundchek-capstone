ef3f997bfcf5a27fec9629bca6bd47ac35cd0dc3
rowena fonseka <rafonseka@gmail.com>
2022-09-25 23:47:11 -0400

    created a modal for rental add page

--- src/pages/RentYourSpace/RentYourSpace.js
+++ src/pages/RentYourSpace/RentYourSpace.js
@@ -1,20 +1,17 @@
 import React, { useEffect, useState } from "react";
 import { db, storage } from "../../firebase";
 import { addDoc, collection } from "firebase/firestore";
-// import { v4 } from "uuid";
-// import {
-//   ref,
-//   uploadBytes,
-//   getDownloadURL,
-//   list,
-//   listAll,
-// } from "firebase/storage";
+import { v4 } from "uuid";
+import { ref, uploadBytes, getDownloadURL } from "firebase/storage";
 import { Link } from "react-router-dom";
 import Header from "../../components/Header/Header";
 import Footer from "../../components/Footer/Footer";
+import RentModal from "../../components/Modals/RentModal/RentModal";
 import "./RentYourSpace.scss";
 
 function RentYourSpace() {
+  const [modalState, setModalState] = useState(false);
+
   const [newCompany, setNewCompany] = useState("");
   const [newAddress, setNewAddress] = useState("");
   const [newName, setNewName] = useState("");
@@ -22,13 +19,14 @@ function RentYourSpace() {
   const [newPeople, setNewPeople] = useState("");
   const [newPrice, setNewPrice] = useState(0);
   const createRoomCollectionRef = collection(db, "rehearsalRooms");
-  //   const [imageUpload, setImageUpload] = useState(null);
-  //   const [imageLink, setImageLink] = useState([]);
-  //   const [imageUrls, setImageUrls] = useState([]);
+
+  const [imageUpload, setImageUpload] = useState(null);
+  const [imageLink, setImageLink] = useState([]);
 
   const createRoom = async (event) => {
     event.preventDefault();
     await addDoc(createRoomCollectionRef, {
+      imgUrl: imageLink,
       company: newCompany,
       address: newAddress,
       name: newName,
@@ -38,65 +36,52 @@ function RentYourSpace() {
     });
   };
 
-  //   const imagesListRef = ref(storage, "images/");
-  //   const uploadImage = () => {
-  //     if (imageUpload == null) return;
-  //     const imageRef = ref(storage, `${imageUpload.name + v4()}`);
-  //     uploadBytes(imageRef, imageUpload).then((snapshot) => {
-  //       getDownloadURL(snapshot.ref).then((url) => {
-  //         setImageUrls((prev) => [...prev, url]);
-  //       });
-  //     });
-  //   };
+  const submitImage = (event) => {
+    setImageUpload(event.target.files[0]);
+  };
 
-  //   useEffect(() => {
-  //     listAll(imagesListRef).then((response) => {
-  //       response.items.forEach((item) => {
-  //         getDownloadURL(item).then((url) => {
-  //           setImageUrls((prev) => [...prev, url]);
-  //         });
-  //       });
-  //     });
-  //   }, []);
+  useEffect(() => {
+    uploadImage();
+  }, [imageUpload]);
 
-  //   const uploadImage = () => {
-  //     if (imageUpload == null) return;
-  //     const imageLink = `${imageUpload.name + v4()}`;
-  //     const imageRef = ref(storage, imageLink);
-  //     // const imageRef = ref(storage, `${imageUpload.name + v4()}`);
-  //     uploadBytes(imageRef, imageUpload).then(() => {
-  //       myImageLink(imageRef);
-  //     });
-  //   };
+  const uploadImage = () => {
+    if (imageUpload == null) return;
+    const imageLink = `${imageUpload.name + v4()}`;
+    const imageRef = ref(storage, imageLink);
+    // const imageRef = ref(storage, `${imageUpload.name + v4()}`);
+    uploadBytes(imageRef, imageUpload).then(() => {
+      myImageLink(imageRef);
+    });
+  };
 
-  //   const myImageLink = (imageRef) => {
-  //     getDownloadURL(imageRef).then((url) => {
-  //       setImageLink(url);
-  //     });
-  //   };
+  const myImageLink = (imageRef) => {
+    getDownloadURL(imageRef).then((url) => {
+      setImageLink(url);
+    });
+  };
 
   return (
     <>
       <Header />
 
-      <h1 className="page-title">Rent your space!</h1>
+      <h1 className="page-title">Rent your space</h1>
       <p className="page-title__subheader">
         So, you're ready to rent out your garage, attic or basement to aspiring
         bands? Simply enter your info below to create a new listing.
       </p>
       <div className="room-container">
         <form className="roomRent" onSubmit={createRoom}>
-          {/* <div className="upload-image">
-          <label className="upload-image__label">Choose Image</label>
-          <input
-            required
-            type="file"
-            onChange={(event) => {
-              setImageUpload(event.target.files[0]);
-            }}
-          />
-          <button onClick={uploadImage}>Upload Image</button>
-        </div> */}
+          <div className="upload-image">
+            <label className="upload-label">Choose Image</label>
+            <input
+              className="upload-label__input"
+              required
+              type="file"
+              onChange={(event) => {
+                submitImage(event);
+              }}
+            />
+          </div>
           <div className="rent">
             <label className="rent__label">Company Name</label>
             <input
@@ -155,9 +140,17 @@ function RentYourSpace() {
               }}
             ></input>
 
-            <button className="rent__btn">Create Rental</button>
+            <button
+              className="rent__btn"
+              onClick={() => {
+                setModalState(true);
+              }}
+            >
+              Create Rental
+            </button>
           </div>
         </form>
+        {modalState && <RentModal setOpenModal={setModalState} />}
       </div>
       <Footer />
     </>
