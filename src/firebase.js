7309fc3e29eba156f5e15df681637bcb091f72ef
rowena fonseka <rafonseka@gmail.com>
2022-09-27 10:14:41 -0400

    removed commented code from firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -10,20 +10,9 @@ const firebaseConfig = {
   storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
   messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
   appId: process.env.REACT_APP_FIREBASE_APP_ID,
-
-  // apiKey: "AIzaSyCjFwfSsg3d_dN6YTNf7efX34xV339905I",
-  // authDomain: "soundchek-capstone.firebaseapp.com",
-  // projectId: "soundchek-capstone",
-  // storageBucket: "soundchek-capstone.appspot.com",
-  // messagingSenderId: "290274258675",
-  // appId: "1:290274258675:web:af0da0d022d74d4e555186",
 };
-
-console.log(process.env.REACT_APP_FIREBASE_API_KEY);
 
 const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
 export const storage = getStorage(app);
 export const auth = getAuth(app);
-
-// export default app;
